1ee8f854d7d4850bcb6ec90a3cdbcdb6754534d9
Buga Grigore <56489233+virusaki97@users.noreply.github.com>
2021-04-19 18:00:08 +0300

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,6 +1,6 @@
 # Date: 02/23/2019
 # Original passgen author: Mohamed
-# Some changed by: UnderMind0x41
+# Some changed by: virusaki
 # Description: A social engineering password generator
 # This script required python version >= 3.0
 import argparse
